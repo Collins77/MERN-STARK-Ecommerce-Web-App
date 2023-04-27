8bfb012313dab93f5a75535a823557c832546c76
Collins77 <collinsmuemah@gmail.com>
2023-04-27 10:31:40 +0300

    Updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,38 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+  <>
+    <footer className='py-4'>
+     <div className='container'>
+      <div className='row align-items-center'>
+        <div className='col-5'>
+          <div className='footer-top-data d-flex gap-30 align-items-center'>
+            <img src="images/newsletter.png" alt="Newsletter"/>
+            <h3 className='mb-0 text-white'>Sign Up for Newsletter</h3>
+          </div>
+        </div>
+        <div className='col-7'>
+              <div className="input-group">
+                  <input type="text" className="form-control py-1" placeholder="Your Email Address" aria-label="Your Email Address" aria-describedby="basic-addon2"/>
+                  <span className="input-group-text p-3" id="basic-addon2">
+                    Subscribe
+                  </span>
+              </div>
+        </div>
+      </div>
+     </div>
+    </footer>
+    <footer className='py-3'></footer>
+    <footer className='py-4'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12'>
+              <p className='text-center mb-0 text-white'>&copy; { new Date().getFullYear()} Powered by Cotek Technologies.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -75,14 +75,26 @@ const Header = () => {
         <div className='container'>
             <div className='row'>
                 <div className='col-12'>
-                    <div className='menu-bottom d-flex align-items-center'>
-                       <div></div> 
+                    <div className='menu-bottom d-flex align-items-center gap-30'>
+                       <div>
+                            <div className="dropdown">
+                                <button className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <img src="images/menu.svg" alt="" />
+                                    <span className='me-5 d-inline-block'>Shop Categories</span>
+                                </button>
+                                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <Link className="dropdown-item text-white" to="#">Action</Link>
+                                    <Link className="dropdown-item text-white" to="#">Another action</Link>
+                                    <Link className="dropdown-item text-white" to="#">Something else here</Link>
+                                </div>
+                            </div>
+                        </div> 
                        <div className='menu-links'>
                             <div className='d-flex align-items-center gap-15'>
-                                <NavLink to='/'>Home</NavLink>
-                                <NavLink to='/'>Our Store</NavLink>
-                                <NavLink to='/'>Blogs</NavLink>
-                                <NavLink to='/contact'>Contact</NavLink>
+                                <NavLink style={{color: 'white', textDecoration: 'none', fontWeight: '400', fontSize: '14px', lineHeight: '18px', letterSpacing: '0.3', textTransform: 'uppercase'}} to='/'>Home</NavLink>
+                                <NavLink style={{color: 'white', textDecoration: 'none', fontWeight: '400', fontSize: '14px', lineHeight: '18px', letterSpacing: '0.3', textTransform: 'uppercase'}} to='/'>Our Store</NavLink>
+                                <NavLink style={{color: 'white', textDecoration: 'none', fontWeight: '400', fontSize: '14px', lineHeight: '18px', letterSpacing: '0.3', textTransform: 'uppercase'}} to='/'>Blogs</NavLink>
+                                <NavLink style={{color: 'white', textDecoration: 'none', fontWeight: '400', fontSize: '14px', lineHeight: '18px', letterSpacing: '0.3', textTransform: 'uppercase'}} to='/contact'>Contact</NavLink>
                             </div>
                         </div> 
                     </div>
